3afd5f2ea3435aced4db84e2ee49128570943f7d
Cyril Junior Dim <Cyril Junior Dim>
2021-01-14 09:29:05 +0100

    Speak now merged with database

--- app/src/main/java/com/example/tabletopics/TopicThinkingActivity.java
+++ app/src/main/java/com/example/tabletopics/TopicThinkingActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,16 +21,25 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class TopicThinkingActivity extends AppCompatActivity {
+    private static final long START_TIME_IN_MILLIS = 30000;
+
     TextView title;
+    TextView timerText;
+    TextView ret;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String theme = "";
     Random random = new Random();
+    Button button;
 
+    private CountDownTimer mCountDownTimer;
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +47,17 @@ public class TopicThinkingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_topic_thinking);
 
         title = (TextView) findViewById(R.id.speechTitle);
+        timerText = (TextView) findViewById(R.id.timertext);
+        ret = (TextView) findViewById(R.id.ret);
+
+        ret.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCountDownTimer.cancel();
+                Intent intent = new Intent(v.getContext(), SpeakNowActivity.class);
+                startActivity(intent);
+            }
+        });
 
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
@@ -61,11 +84,33 @@ public class TopicThinkingActivity extends AppCompatActivity {
                             int randomIndex = random.nextInt(topics.size());
                             String randomElement = topics.get(randomIndex);
                             title.setText(randomElement);
+                            startTimer();
                         } else{
                             Log.w("TAG", "Error getting documents.", task.getException());
                         }
 
                     }
                 });
+    }
+
+    private void startTimer() {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+            @Override
+            public void onFinish() {
+                timerText.setVisibility(View.GONE);
+            }
+        }.start();
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        timerText.setText(timeLeftFormatted);
     }
 }
