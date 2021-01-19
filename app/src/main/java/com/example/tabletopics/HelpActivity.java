9972950942d7148b2213551606c36b4478e0163e
Cyril Junior Dim <Cyril Junior Dim>
2021-01-19 13:50:03 +0100

    Help Page Template Created

---
+++ app/src/main/java/com/example/tabletopics/HelpActivity.java
@@ -0,0 +1,34 @@
+package com.example.tabletopics;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.Objects;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+        Toolbar myToolbar=findViewById(R.id.my_toolbar);
+        myToolbar.setTitle("Help");
+        myToolbar.setTitleTextColor(0xFFFFFFFF);
+        setSupportActionBar(myToolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        myToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                finish();
+            }
+        });
+    }
+}
